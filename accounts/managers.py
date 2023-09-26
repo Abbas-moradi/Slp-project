34b5f489c8dbf773df17539702456d42f2bde76c
Abbas moradi <en.moradi.66@gmail.com>
2023-09-27 00:06:50 +0330

    improve user manager

--- accounts/managers.py
+++ accounts/managers.py
@@ -10,8 +10,6 @@ class UserManager(BaseUserManager):
             raise ValueError('user must have email')
         if not full_name:
             raise ValueError('user must have full name')
-        if not password:
-            raise ValueError('user must have password')
         
         user = self.model(phone=phone, email=self.normalize_email(email), full_name=full_name)
         user.set_password(password)
