aa1fdfce597e3c05e65948394e00d003f93cdf12
Abbas moradi <en.moradi.66@gmail.com>
2023-09-27 20:59:09 +0330

    add user register view

--- accounts/views.py
+++ accounts/views.py
@@ -6,4 +6,8 @@ from .forms import UserRegisterationForm
 class UserRegisterView(View):
     form_class = UserRegisterationForm
 
+    def get(self, request):
+        pass
 
+    def post(self, request):
+        pass
