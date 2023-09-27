53aafe23137e9ecd94682327895bd11f26749e95
Abbas moradi <en.moradi.66@gmail.com>
2023-09-27 19:55:02 +0330

    add register page

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.views import View
+from .forms import UserRegisterationForm
 
 
 class UserRegisterView(View):
-    pass
+    form_class = UserRegisterationForm
+    
+    def get(self, request):
+        form = self.form_class
+        return render(request, 'accounts/register.html', {'form': form})
+
+    def post(self, request):
+        pass
