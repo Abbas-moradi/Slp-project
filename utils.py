943e51847229c31569be41513d70479c7e50a2fb
Abbas moradi <en.moradi.66@gmail.com>
2023-09-29 12:14:28 +0330

    impelement sender otp code

--- utils.py
+++ utils.py
@@ -1,2 +1,14 @@
+from kavenegar import *
+
 def send_otp_code(phone, code):
-    pass
+    try:
+        api = KavenegarAPI('2F77537067544365733452714E42516D69344B6176456854354732787363716B4B7848316F5165525155553D')
+        params = { 'sender' : '1000689696',
+                   'receptor': phone,
+                   'message' :f' :کد تایید شما {code}' }
+        
+        response = api.sms_send( params)
+    except APIException as e:
+        print(e)
+    except HTTPException as h:
+        print(h)
