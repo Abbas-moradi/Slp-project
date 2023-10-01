8bbba089ff842a545f1e5707b998b115241d00df
Abbas moradi <en.moradi.66@gmail.com>
2023-10-02 00:07:12 +0330

    category and article model implement

--- home/models.py
+++ home/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Articles(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField()
+    image = models.ImageField(upload_to='posts/', null=True, blank=True)
+    tags = models.CharField(max_length=255)
+    author = models.CharField(max_length=150)
+    created = models.DateField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=True)
+    is_delete = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return f'{self.title} - {self.author} - {self.created}'
+    
+
+class Category(models.Model):
+    name = models.CharField(max_length=150)
+    favicon = models.CharField(max_length=30, null=True, blank=True)
+    description = models.CharField(max_length=250, blank=True)
+    color_status = models.CharField(max_length=30)
+    image = models.ImageField(upload_to='category/', null=True, blank=True)
+    status = models.BooleanField(default=True)
+
+    def __str__(self) -> str:
+        return f'{self.name} - {self.favicon} - {self.description} - {self.status}'
+    
+
